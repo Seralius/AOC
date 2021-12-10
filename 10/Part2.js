e240fa02390ddadc61843f3bd5f2d3a595b78712
Seralius <develop.seralius@outlook.com>
2021-12-10 07:54:41 +0100

    Solved Day 10

--- 10/Part2.js
+++ 10/Part2.js
@@ -3,7 +3,82 @@ const fs = require('fs')
 var Rawinput = fs.readFileSync("input.txt").toString('utf-8')
 input = Rawinput.split("\r\n");
 function Run() {
-    
+    var OpenChars = "([{<"
+    var CloseChars = ")]}>"
+    var Sums = []
+    var Test = "";
+    input.forEach(function (line) {
+        var first = true
+        var LineString = "";
+        for (let i = 0; i < line.length; i++) {
+            const element = line[i];
+            if (OpenChars.includes(element)) {
+                LineString += element;
+            }
+            else {
+                var Expected = BuildString(LineString);
+                if (Expected[i] != element && first) {
+                    first = false
+                    //     Sum += element == ")" ? 3 :
+                    //         element == "]" ? 57 :
+                    //             element == "}" ? 1197 :
+                    //                 element == ">" ? 25137 :
+                    //                     0;
+                }
+                else {
+                    LineString += element;
+                }
+            }
+        }
+        if (first) {
+            var Score = 0;
+            var BuiltString = BuildString(line);
+            if (BuiltString.length > line.length) {
+                var Diff = BuiltString.slice(line.length, BuiltString.length)
+                Diff.split("").forEach(function (char) {
+                    Score = Score * 5;
+                    switch (char) {
+                        case ")":
+                            Score += 1;
+                            break;
+                        case "]":
+                            Score += 2;
+                            break;
+                        case "}":
+                            Score += 3;
+                            break;
+                        case ">":
+                            Score += 4;
+                            break;
+                    }
+                })
+            }
+            if (Diff.length > 0) {
+                Sums.push(Score)
+            }
+        }
+    })
+    Sums = Sums.sort(function (a, b) { return a - b })
+    console.log(Sums[parseInt(Sums.length / 2)])
+    function BuildString(input) {
+        var selfbuilt = "";
+        var closed = "";
+        input = input.split("");
+        input.forEach(function (element) {
+            if (OpenChars.includes(element)) {
+                selfbuilt += element;
+                closed = closed.split("").reverse().join("")
+                closed += CloseChars[OpenChars.indexOf(element)];
+                closed = closed.split("").reverse().join("")
+            }
+            else {
+                selfbuilt += element;
+                closed = closed.slice(1);
+            }
+        })
+        selfbuilt += closed;
+        return selfbuilt;
+    }
 }
 module.exports = Run;
 //Start timer
